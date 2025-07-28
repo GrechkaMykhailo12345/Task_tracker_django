f407b6911f611697941448bb713cb6725c25d97f
GrechkaMykhailo12345 <yalta010709@gmail.com>
2025-07-28 19:14:07 +0300

    Додав функціональність редагування та видалення, додав автентифікацію користувача, додав фільтрацію і додав деякі дизайни

--- tasktrackerapp/urls.py
+++ tasktrackerapp/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from tasktrackerapp.views import TaskListView, TaskDetailView, TaskCreateView
+from tasktrackerapp.views import TaskListView, TaskDetailView, TaskCreateView, TaskCompleteView, TaskUpdateView, TaskDeleteView
 
 urlpatterns = [
     path('', TaskListView.as_view(), name="task_list"),
     path('task/<int:pk>/detail', TaskDetailView.as_view(), name="task_detail"),
     path('task_create', TaskCreateView.as_view(), name="task_create"),
+    path('task/<int:pk>/complete', TaskCompleteView.as_view(), name="task_complete"),
+    path('task/<int:pk>/update', TaskUpdateView.as_view(), name="task_update"),
+    path('task/<int:pk>/delete', TaskDeleteView.as_view(), name="task_delete"),
 ]
 
 app_name = "tasktrackerapp"
--- tasktrackerapp/views.py
+++ tasktrackerapp/views.py
@@ -1,18 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView
-from tasktrackerapp.forms import TaskForm
+from django.views.generic import ListView, DetailView, CreateView, View, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from tasktrackerapp import models
+from tasktrackerapp.mixins import UserIsOwnerMixin
+from tasktrackerapp.forms import TaskForm, TaskFilterForm
 from tasktrackerapp.models import Task
+from django.http import HttpResponseRedirect
 
 class TaskListView(ListView):
     model = Task
     template_name = "task_list.html"
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        status = self.request.GET.get("status", "")
+        if status:
+            queryset = queryset.filter(status=status)
+        return queryset
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = TaskFilterForm(self.request.GET)
+        return context
+
 class TaskDetailView(DetailView):
     model = Task
     template_name = "task_detail.html"
 
-class TaskCreateView(CreateView):
+class TaskCreateView(LoginRequiredMixin, CreateView):
     model = Task
     template_name = "task_form.html"
     form_class = TaskForm
@@ -21,3 +37,25 @@ class TaskCreateView(CreateView):
     def form_valid(self, form):
         form.instance.created_by = self.request.user
         return super().form_valid(form)
+
+class TaskCompleteView(LoginRequiredMixin, UserIsOwnerMixin, View):
+    def post(self, request, *args, **kwargs):
+        task = self.get_object()
+        task.status = "done"
+        task.save()
+        return HttpResponseRedirect(reverse_lazy("tasktrackerapp:task_list"))
+    
+    def get_object(self):
+        task_id = self.kwargs.get("pk")
+        return get_object_or_404(models.Task, pk=task_id)
+
+class TaskUpdateView(LoginRequiredMixin, UserIsOwnerMixin, UpdateView):
+    model = Task
+    form_class = TaskForm
+    template_name = "task_update_form.html"
+    success_url = reverse_lazy("tasktrackerapp:task_list")
+
+class TaskDeleteView(LoginRequiredMixin, UserIsOwnerMixin, DeleteView):
+    model = Task
+    template_name = "task_delete_confirmation.html"
+    success_url = reverse_lazy("tasktrackerapp:task_list")
